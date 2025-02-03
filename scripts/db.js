3a72e3eb790dc78756e1ef35f7633948b1fff9f2
Marcel Göhn <marcelgoehn@googlemail.com>
2025-02-03 15:45:34 +0100

    added dishes + footer

---
+++ scripts/db.js
@@ -0,0 +1,62 @@
+let dishes = [
+    {
+        "name": "GIROS",
+        "ingredients": "mit Reis und einem großen Krautsalat",
+        "price": 15.30
+    },
+    {
+        "name": "ATHEN-PLATTE",
+        "ingredients": "1 Lammfilet, 3 Stück Leber, 2 Souflaki, Reis und Krautsalat",
+        "price": 17.80
+    },
+    {
+        "name": "OLYMPIA-PLATTE",
+        "ingredients": "2 Souflaki, 2 Stück Leber, Reis und Krautsalat",
+        "price": 18.10
+    },
+    {
+        "name": "KALAMARIS",
+        "ingredients": "Tintenfisch mit Reis und Pommes",
+        "price": 18.80
+    },
+    {
+        "name": "PIZZA PAZZA",
+        "ingredients": "Créme fraiche, Ziegenkäse, Parmaschinken, Feige, Rucola, Walnuss, Honig",
+        "price": 16.90
+    },
+    {
+        "name": "LASAGNE AL FORNO",
+        "ingredients": "Mit Schinken, Bolognese und Käse überbacken",
+        "price": 13.90
+    },
+    {
+        "name": "TIRAMISU",
+        "ingredients": "Der Klassiker",
+        "price": 9.90
+    },
+    {
+        "name": "GALAKTOBURIKO",
+        "ingredients": "gefüllter Eiercremeblätterteig mit Vanilleeis",
+        "price": 7.80
+    },
+    {
+        "name": "Vanille-Eis",
+        "ingredients": "mit Schokosauce",
+        "price": 6.20
+    },
+    {
+        "name": "OUZO",
+        "ingredients": "2cl",
+        "price": 3.50
+    },
+    {
+        "name": "METAXA 5 Sterne",
+        "ingredients": "2cl",
+        "price": 5.50
+    },
+    {
+        "name": "Amethystos Rose",
+        "ingredients": "0,75 l Flasche",
+        "price": 36.50
+    }
+];
